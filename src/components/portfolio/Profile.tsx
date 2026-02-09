a75ef6d0e760a49c05045411604cd632e5d540ff
M-GFD <mateogfd35@gmail.com>
2026-02-09 19:39:09 -0300

    fixed texts content details

--- src/components/portfolio/Profile.tsx
+++ src/components/portfolio/Profile.tsx
@@ -12,12 +12,12 @@ export default function Profile() {
             />
           </div>
           <div className="flex-1 text-justify md:text-left">
-            <h3 className="text-4xl font-bold text-black mb-4">what about me?</h3>
+            <h3 className="text-4xl font-bold text-black mb-4">What about me???</h3>
             <p className="text-justify text-gray-600 mb-6">
-              my name is Mateo and I'm a designer with expertise in creating digital experiences that connect brands with their audiences. With years of experience in Graphic Design and UI/UX Design, I transform ideas into reality.
+              My name is Mateo and I'm a designer with expertise in creating digital experiences that connect brands with their audiences. With years of experience in Graphic Design and UI/UX Design, I transform ideas into reality.
             </p>
             <p className="text-justify text-gray-600">
-              my approach: user-centered creative thinking with technical skills, ensuring every project not only looks great but also performs really well at the same time.
+              My approach: user-centered creative thinking with technical skills, ensuring every project not only looks great but also performs really well at the same time.
             </p>
           </div>
         </div>
